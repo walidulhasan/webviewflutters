30017a3776a4acc513af215459f91fc6adb3f39f
walidulhasan <walidulhasanboniamin@gmail.com>
2022-11-15 20:17:27 +0600

    webviewsflutterCheck

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  var loadingPercentage = 0;
+  late WebViewController _webViewController;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WillPopScope(
+        onWillPop: () async {
+          if (await _webViewController.canGoBack()) {
+            _webViewController.goBack();
+            return false;
+          } else {
+            return true;
+          }
+        },
+        child: SafeArea(
+          child: WebView(
+            initialUrl: 'http://rotary.safeerp.xyz/index.aspx',
+            onPageStarted: (url) {
+              setState(() {
+                loadingPercentage = 0;
+              });
+            },
+            onProgress: (progress) {
+              setState(() {
+                loadingPercentage = progress;
+              });
+            },
+            onPageFinished: (url) {
+              setState(() {
+                loadingPercentage = 100;
+              });
+            },
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _webViewController = webViewController;
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
